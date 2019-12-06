23da48ad963aaa80e919237452e5d6bde5a1f06d
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2019-12-06 10:33:26 +0100

    changing files

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -8,13 +8,13 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\SkeletonBundle\ContaoManager;
+namespace maniax-at-work\ContaoPortfolioBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\SkeletonBundle\ContaoSkeletonBundle;
+use maniax-at-work\ContaoPortfolioBundle\ContaoPortfolioBundle;
 
 class Plugin implements BundlePluginInterface
 {
--- src/ContaoPorfolioBundle.php
+++ src/ContaoPorfolioBundle.php
@@ -8,10 +8,10 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\SkeletonBundle;
+namespace maniax-at-work\ContaoPortfolioBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ContaoSkeletonBundle extends Bundle
+class ContaoPortfolioBundle extends Bundle
 {
 }
